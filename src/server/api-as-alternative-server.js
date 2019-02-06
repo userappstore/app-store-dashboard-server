2b8bebdd473551d36232843080142246435244a3
Ben Lowry <benlowry@outlook.com>
2019-02-06 08:45:53 -0600

    Removed debugging info

--- src/server/api-as-alternative-server.js
+++ src/server/api-as-alternative-server.js
@@ -1,18 +1,10 @@
 const applicationServer = require('../application-server.js')
-global.api = {}
-global.ui = {}
+
 // This reconfigures API requests made by the application server
 // using a user's application server and superimposes the new
 // configuration on the request
 module.exports = {
   before: async (req) => { 
-    if (req.urlPath.startsWith('/api/')) {
-      global.api[req.urlPath] = global.api[req.urlPath] || 0
-      global.api[req.urlPath]++
-    } else if (!req.urlPath.startsWith('/public/')) {
-      global.ui[req.urlPath] = global.ui[req.urlPath] || 0
-      global.ui[req.urlPath]++
-    }
     if (!req.headers['x-application-server'] || req.headers['x-application-server'] === global.applicationServer) {
       return
     }
