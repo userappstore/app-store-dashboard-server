9d0050ed68a464993e387dcc83d8c04edb72fb37
Ben Lowry <benlowry@outlook.com>
2019-03-19 09:41:30 -0600

    Use original handlers

--- src/www/api/application-server/update-default-profile.js
+++ src/www/api/application-server/update-default-profile.js
@@ -1,7 +1,6 @@
-const dashboard = require('@userappstore/dashboard')
-// This is largely a copy/paste it just doesn't
-// lock the session to authorize updating for
-// the application server to update profiles
+const updateProfile = require('@userappstore/dashboard/src/www/api/user/update-profile.js')
+updateProfile.lock = false
+
 module.exports = {
   patch: async (req) => {
     if (!req.applicationServer) {
@@ -10,38 +9,10 @@ module.exports = {
     if (!req.query || !req.query.accountid) {
       throw new Error('invalid-accountid')
     }
-    const account = await global.api.administrator.Account._get(req)
-    req.query.profileid = account.profileid
-    const profile = await global.api.user.Profile._get(req)
-    if (!profile) {
-      throw new Error('invalid-profileid')
+    if (req.query.accountid !== req.account.accountid) {
+      throw new Error('invalid-account')
     }
-    if (!req.body || !req.body.email || !req.body.email.length || !req.body.email.indexOf('@') > 0) {
-      throw new Error('invalid-profile-email')
-    }
-    if (!req.body['first-name'] || !req.body['first-name'].length) {
-      throw new Error('invalid-profile-first-name')
-    }
-    if (global.minimumProfileFirstNameLength > req.body['first-name'].length ||
-      global.maximumProfileFirstNameLength < req.body['first-name'].length) {
-      throw new Error('invalid-profile-first-name-length')
-    }
-    if (!req.body['last-name'] || !req.body['last-name'].length) {
-      throw new Error('invalid-profile-last-name')
-    }
-    if (global.minimumProfileLastNameLength > req.body['last-name'].length ||
-      global.maximumProfileLastNameLength < req.body['last-name'].length) {
-      throw new Error('invalid-profile-last-name-length')
-    }
-    await dashboard.StorageObject.setProperties(`${req.appid}/profile/${req.query.profileid}`, {
-      firstName: req.body['first-name'],
-      lastName: req.body['last-name'],
-      email: req.body.email
-    })
-    req.success = true
-    profile.firstName = req.body['first-name']
-    profile.lastName = req.body['last-name']
-    profile.email = req.body.email
-    return profile
+    req.query.profileid = req.account.profileid
+    return updateProfile.patch(req)
   }
 }
