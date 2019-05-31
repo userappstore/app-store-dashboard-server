5c214e7cf1ae70f82d2a9eba6b7656b3a74c3613
Ben Lowry <benlowry@outlook.com>
2019-05-31 11:15:26 -0600

    Don't create blank routes

--- src/server/remove-template-after-authorization.js
+++ src/server/remove-template-after-authorization.js
@@ -1,7 +1,9 @@
 module.exports = {
   after: (req) => {
+    if (!req.route) {
+      return
+    }
     if (req.query && req.query.removeTemplate) {
-      console.log('removing template', req.query, req.session.unlocked)
       const routeWas = req.route
       req.route = {}
       for (const key in routeWas) {
