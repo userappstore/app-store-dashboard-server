f01d540a64877351d688a7a3d24be60ab8f0887d
Ben Lowry <benlowry@outlook.com>
2019-06-21 14:56:48 -0600

    WIP bridge for projects to access API from iframe (no cookie)

--- src/server/installid-in-url.js
+++ src/server/installid-in-url.js
@@ -2,6 +2,33 @@ const applicationServer = require('../application-server.js')
 const dashboard = require('@userappstore/dashboard')
 
 module.exports = {
+  before: async (req) => {
+    if (!req.query || !req.query.accountid || !req.query.sessionid || !req.query.token) {
+      return
+    }
+    if (!req.urlPath.startsWith('/api/')) {
+      return
+    }
+    const urlParts = req.urlPath.split('/')
+    if (urlParts.length < 4) {
+      return
+    }
+    const installid = urlParts[3]
+    if (installid === 'user' || installid === 'administrator') {
+      return
+    }
+    let data = await dashboard.Storage.read(`map/applicationSessionTokens/${req.query.token}`)
+    data = JSON.parse(data)
+    req.data = data
+    const queryWas = req.query
+    req.query = {}
+    req.query.sessionid = data.sessionid
+    req.session = await global.api.administrator.Session._get(req)
+    req.query.accountid = req.session.accountid
+    req.account = await global.api.administrator.Account._get(req)
+    req.query = queryWas
+    req.allowAPIRequest = true
+  },
   // This detects requests made by a user within an
   // install or the account settings for it, to do that
   // they need a session created under the account for 
@@ -9,66 +36,102 @@ module.exports = {
   // repurposed before the request proceeds to the
   // UserAppStore backend to load the project or URL
   after: async (req, res) => {
-    if (!req.urlPath.startsWith('/install/') && !req.urlPath.startsWith('/account/')) {
+    if (!req.urlPath.startsWith('/install/')
+      && !req.urlPath.startsWith('/account/')
+      && !req.urlPath.startsWith('/api/')) {
       return
     }
-    if (!req.account || req.route) {
+    if (req.route) {
       return
     }
-    const installid = req.urlPath.split('/')[2]
-    const sessionWas = req.session
+    if (!req.account) {
+      return
+    }
+    const urlParts = req.urlPath.split('/')
+    if (urlParts.length < 4) {
+      return
+    }
+    let installid = req.urlPath.startsWith('/api/') ? urlParts[3] : urlParts[2]
+    if (installid === 'user' || installid === 'administrator') {
+      return
+    }
+    let sessionid, install
     const queryWas = req.query
     const bodyWas = req.body
-    let install
-    try {
-      install = await applicationServer.get(`/api/dashboard-server/install?installid=${installid}`, req.account.accountid, req.session.sessionid)
-    } catch (error) {
-    }
-    if (!install) {
+    const sessionWas = req.session
+    if (req.query && req.query.accountid && req.query.sessionid && req.query.token) {
+      req.account = { accountid: req.data.accountid }
+      req.session = { sessionid: req.data.sessionid }
+      req.install = await applicationServer.get(`/api/dashboard-server/install?installid=${installid}`, req.account.accountid, req.session.sessionid)
+      req.server = await applicationServer.get(`/api/user/userappstore/application-server?serverid=${req.install.serverid}`, req.account.accountid, req.session.sessionid)
+      req.account = { accountid: req.query.accountid }
+      req.session = { sessionid: req.query.sessionid }
+      req.appid = req.install.serverid
+      const newURL = req.url = req.url.replace(`/${req.install.installid}/`, '/')
+      req.urlPath = req.url.split('?')[0]
+      req.route = global.sitemap[newURL]
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+      res.setHeader('Access-Control-Request-Headers', 'X-Requested-With, accept, content-type')
+      req.url = req.url.replace(`/${installid}/`, `/`)
+      req.urlPath = req.url.split('?')[0]
+      req.route = global.sitemap[req.urlPath]
+      req.query = queryWas
       return
-    }
-    if (install.url && !install.serverid) {
-      req.query = { installid: install.installid }
-      req.route = global.sitemap['/install/home']
-      return
-    }
-    let sessionid = await dashboard.Storage.read(`map/applicationSession/${sessionWas.sessionid}/${install.serverid}`)
-    req.server = await applicationServer.get(`/api/user/userappstore/application-server?serverid=${install.serverid}`, req.account.accountid, req.session.sessionid)
-    req.appid = install.serverid
-    if (req.server.stripeid) {
-      req.stripeKey = {
-        api_key: req.stripeKey.api_key,
-        stripe_account: req.server.stripeid
-      }
-    }
-    if (!sessionid) {
-      req.body = {
-        username: `user-${install.serverid}-${req.account.accountid}`,
-        password: 'password'
-      }
-      let session
+    } else {
       try {
-        session = await global.api.user.CreateSession._post(req)
+        install = await applicationServer.get(`/api/dashboard-server/install?installid=${installid}`, req.account.accountid, req.session.sessionid)
       } catch (error) {
       }
-      if (!session) {
-        res.statusCode = 302
-        res.setHeader('location', `/setup-install-profile?installid=${install.installid}`)
-        res.ended = true
-        return res.end()
+      if (!install) {
+        return
       }
-      delete (req.success)
-      await dashboard.StorageObject.setProperty(`${req.appid}/session/${session.sessionid}`, 'expires', sessionWas.expires)
-      await dashboard.Storage.write(`map/applicationSession/${sessionWas.sessionid}/${install.serverid}`, session.sessionid)
-      sessionid = session.sessionid
+      if (install.url && !install.serverid) {
+        req.query = { installid: install.installid }
+        req.route = global.sitemap['/install/home']
+        return
+      }
+      sessionid = await dashboard.Storage.read(`map/applicationSession/${sessionWas.sessionid}/${install.serverid}`)
+      req.server = await applicationServer.get(`/api/user/userappstore/application-server?serverid=${install.serverid}`, req.account.accountid, req.session.sessionid)
+      req.appid = install.serverid
+      if (req.server.stripeid) {
+        req.stripeKey = {
+          api_key: req.stripeKey.api_key,
+          stripe_account: req.server.stripeid
+        }
+      }
+      if (!sessionid) {
+        req.body = {
+          username: `user-${install.serverid}-${req.account.accountid}`,
+          password: 'password'
+        }
+        let newSession
+        try {
+          newSession = await global.api.user.CreateSession._post(req)
+        } catch (error) {
+        }
+        if (!newSession) {
+          res.statusCode = 302
+          res.setHeader('location', `/setup-install-profile?installid=${install.installid}`)
+          res.ended = true
+          return res.end()
+        }
+        delete (req.success)
+        await dashboard.StorageObject.setProperty(`${req.appid}/session/${newSession.sessionid}`, 'expires', sessionWas.expires)
+        await dashboard.StorageObject.setProperty(`${req.appid}/session/${newSession.sessionid}`, 'token', newSession.token)
+        await dashboard.Storage.write(`map/applicationSessionTokens/${newSession.token}`, {
+          sessionid: sessionWas.sessionid,
+          accountid: sessionWas.accountid,
+          serverid: install.serverid,
+          installid: install.installid
+        })
+        await dashboard.Storage.write(`map/applicationSession/${sessionWas.sessionid}/${install.serverid}`, newSession.sessionid)
+        sessionid = newSession.sessionid
+      }
     }
-    let session
     req.query = { sessionid }
-    try {
-      session = await global.api.administrator.Session._get(req)
-    } catch (error) {
-    }
-
+    let session = await global.api.administrator.Session._get(req)
     req.install = install
     if (req.urlPath.startsWith('/account/')) {
       req.urlPath = req.urlPath.split(`/${installid}`).join(``)
---
+++ src/www/install/whois.js.js
@@ -0,0 +1,16 @@
+module.exports = {
+  get: async (req, res) => {
+    req.query = req.query | {}
+    req.query.accountid = req.session.accountid
+    const user = {}
+    user.sessionid = req.session.sessionid
+    user.accountid = req.session.accountid
+    user.installid = req.install.installid
+    user.token = req.session.token
+    if (req.install.organizationid) {
+      user.organizationid = req.install.organizationid
+    }
+    res.setHeader('content-type', 'application/javascript')
+    return res.end('window.user = ' + JSON.stringify(user))
+  }
+}
